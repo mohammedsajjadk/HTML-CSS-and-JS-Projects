bc656de29a1ce0900ca89f5da3ff7d61f790f9d5
Mohammed Sajjad K <mohammedsajjadk@gmail.com>
2020-09-26 12:15:13 +0100

    Header

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "public/scripts"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+            plugins: ["transform-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/scripts/",
+    watchContentBase: true,
+  },
+  devtool: "source-map",
+};
