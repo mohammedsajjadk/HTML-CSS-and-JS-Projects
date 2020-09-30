0e67984ed62c116fedf10ad832d9b896b901d49b
Mohammed Sajjad K <mohammedsajjadk@gmail.com>
2020-09-30 06:15:57 +0100

    Added Babel & changed project structure

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
 import * as likesView from "./views/likesView";
 import { elements, renderLoader, clearLoader } from "./views/base";
-
 /** Global state of the app
  * - Search object
  * - Current recipe object
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["babel-polyfill", "./src/js/index.js"],
+  entry: ["babel-polyfill", "./src/js/index.js"], // For Babel - Step 4: add "babel-polyfill".
+  // So, this is one huge bundle which will have polyfills for >=ES6 code and our js code as well
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "scripts/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "public"), //this is the one which we will be giving to client
-    publicPath: "/scripts/",
+    /*  contentBase: path.resolve(__dirname, "dist"), 
+    publicPath: "/scripts/", */
+    contentBase: "./dist", //this is the one which we will be giving to client
     watchContentBase: true,
   },
   plugins: [
@@ -21,10 +23,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.js$/, // For Babel - Step 1: Check if the file is a .js, if yes, then it will apply babel-loader
+        exclude: /node_modules/, // For Babel - Step 2: If we don't exclude, babel will be applied to all js files inside node_modules
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader", // For Babel - Step 3: converting sass to css, es6 to es5 etc. Also, we have .babelrc file where we will
+          // mention what are the browser version we are targeting.
           options: {
             presets: ["env"],
             plugins: ["transform-object-rest-spread"],
