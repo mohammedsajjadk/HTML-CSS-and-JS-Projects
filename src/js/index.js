3fe4d4440ceb62d6e1357a1e56c6d215fb83a30a
Mohammed Sajjad K <mohammedsajjadk@gmail.com>
2020-10-01 06:37:01 +0100

    search results etc.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-
+import { elements, renderLoader, clearLoader } from "./views/base";
 const state = {};
 
 const controlSearch = async () => {
@@ -12,12 +12,16 @@ const controlSearch = async () => {
     state.search = new Search(query);
 
     // 3) Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+    
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
-    console.log(state.search.result);
+    clearLoader();
+    searchView.renderResults(state.search.result);
   }
 };
 
