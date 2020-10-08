3cb8fa108c7df481792995bb50255b3ceafd2cef
Mohammed Sajjad K <mohammedsajjadk@gmail.com>
2020-10-08 07:12:35 +0100

    Initial Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,37 @@
-// import Search from "./models/Search";
-// import * as searchView from "./views/searchView";
-// import { elements, renderLoader, clearLoader } from "./views/base";
+import { renderTodos } from "./views";
+import { setFilters } from "./filters";
+import { createTodo, loadTodos } from "./todos";
+debugger;
+renderTodos();
 
-/** Global state of the app
- */
-// const state = {};
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  setFilters({
+    searchText: e.target.value,
+  });
+  renderTodos();
+});
 
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  const text = e.target.elements.text.value.trim();
+  e.preventDefault();
 
-// Write the code for your controllers here!
+  if (text.length > 0) {
+    createTodo(text);
+    renderTodos();
+    e.target.elements.text.value = "";
+  }
+});
+
+document.querySelector("#hide-completed").addEventListener("change", (e) => {
+  setFilters({
+    hideCompleted: e.target.checked,
+  });
+  renderTodos();
+});
+
+window.addEventListener("storage", (e) => {
+  if (e.key === "todos") {
+    loadTodos();
+    renderTodos();
+  }
+});
