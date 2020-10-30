2c5c53991f68d5284679aec4b09ffe219e578d57
Mohammed Sajjad K <you@example.com>
2020-10-30 15:41:24 +0000

    validateForm

--- src/js/index.js
+++ src/js/index.js
@@ -104,6 +104,10 @@ DOMstrings.stepsForm.addEventListener('click', e => {
   //find active panel
   const activePanel = findParent(eventTarget, `${DOMstrings.stepFormPanelClass}`);
   let activePanelNum = Array.from(DOMstrings.stepFormPanels).indexOf(activePanel);
+  if(eventTarget.classList.contains(`${DOMstrings.stepNextBtnClass}`) && !validateForm(activePanelNum)) {
+    return;
+  } 
+
   //set active step and active panel onclick
   if (eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) {
       activePanelNum--;
@@ -125,25 +129,27 @@ document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
   console.log('Submitted')
 })
-// YES TO IMPLEMENT
-// function validateForm() {
-//   // This function deals with validation of the form fields
-//   var x, y, i, valid = true;
-//   x = document.getElementsByClassName("tab");
-//   y = x[currentTab].getElementsByTagName("input");
-//   // A loop that checks every input field in the current tab:
-//   for (i = 0; i < y.length; i++) {
-//     // If a field is empty...
-//     if (y[i].value == "") {
-//       // add an "invalid" class to the field:
-//       y[i].className += " invalid";
-//       // and set the current valid status to false
-//       valid = false;
-//     }
-//   }
-//   // If the valid status is true, mark the step as finished and valid:
-//   if (valid) {
-//     document.getElementsByClassName("step")[currentTab].className += " finish";
-//   }
-//   return valid; // return the valid status
-// }
+
+function validateForm(activePanelNum) {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("multisteps-form__content");
+  y = x[activePanelNum].querySelectorAll("input[required]");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    } else {
+      y[i].classList.remove("invalid");
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  // if (valid) {
+  //   document.getElementsByClassName("multisteps-form__content")[activePanelNum].className += " finish";
+  // }
+  return valid; // return the valid status
+}
