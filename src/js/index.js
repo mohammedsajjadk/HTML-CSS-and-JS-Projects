96b44bbeb4ea9fe3ce1e26e040a2348faf793231
Mohammed Sajjad K <you@example.com>
2020-11-07 13:22:50 +0000

    JS and Prod build

--- src/js/index.js
+++ src/js/index.js
@@ -3,28 +3,33 @@ const navigationEle = document.querySelector('.navigation')
 const imageContainerEle = document.querySelectorAll('.image-container img')
 const textBoxEle = document.querySelector('.text-box')
 
-function toggleSrc(newColor) {
+function switchTheme(newColor) {
+    // Setting to local storage
     localStorage.setItem('theme', newColor)
-    const currentColor = newColor === 'light' ? 'dark' : 'light';
+   
+     // These are the things we are changing when we change the theme
+     const currentColor = newColor === 'light' ? 'dark' : 'light';
+     document.documentElement.setAttribute('data-theme', newColor) // setting to HTML
+     imageContainerEle.forEach(curr => curr.src = curr.src.replace(currentColor, newColor))
+   
     const newNavBgColor = newColor === 'light' ? 'rgba(255, 255, 255, 0.5)' : 'rgba(0, 0, 0, 0.5)';
-    const newtoggleBgColor = newColor === 'light' ? 'rgba(66, 66, 66, 0.6)' : 'rgba(255, 255, 255, 0.5)';
-    
-    document.documentElement.setAttribute('data-theme', newColor)
-    imageContainerEle.forEach(curr => curr.src = curr.src.replace(currentColor, newColor))
     navigationEle.style.background = newNavBgColor;
+
+    const newtoggleBgColor = newColor === 'light' ? 'rgba(66, 66, 66, 0.6)' : 'rgba(255, 255, 255, 0.5)';
     textBoxEle.style['background-color'] = newtoggleBgColor;
 }
 
-
 checkboxEle.addEventListener('change', e => {
     const isChecked = e.target.checked;
-    if(isChecked) {
-        toggleSrc('dark');
+    if (isChecked) {
+        switchTheme('dark');
     } else {
-        toggleSrc('light');
+        switchTheme('light');
     }
 })
 
-if(localStorage.getItem('theme') === 'dark') {
-    toggleSrc('dark');
+
+/* Checking at the start, from the localstorage, if the theme is dark or not */
+if (localStorage.getItem('theme') === 'dark') {
+    switchTheme('dark');
 }
